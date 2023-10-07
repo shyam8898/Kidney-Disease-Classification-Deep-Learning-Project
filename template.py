85c7d304ada51ae21d91783d2f201b5f6cda10f1
shyam8898 <shyampatel801@gmal.com>
2023-10-07 18:26:30 +0530

    utils added

--- template.py
+++ template.py
@@ -18,6 +18,7 @@ list_of_files = [
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
+    "main.py"
     "dvc.yaml",
     "params.yaml",
     "requirements.txt",
